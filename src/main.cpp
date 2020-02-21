bfc5f97240aaef1e27ee3dca66561d5440c3c354
wjchenqing <wjchenqing@sjtu.edu.cn>
2020-02-22 02:33:53 +0800

    2.22 Debug. Fix Function

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@ using namespace antlr4;
 int main(int argc, const char* argv[]){
     //todo:please don't modify the code below the construction of ifs if you want to use visitor mode
 //    const std::string filepath=argv[1];
-//    const std::string filepath="testData/test2.in";
-//    std::ifstream ifs;
-//    ifs.open(filepath);
-//    if (!ifs.good()) {
-//        std::cout<<"bad";
-//    }
-//    ANTLRInputStream input(ifs);
-    ANTLRInputStream input(std::cin);
+    const std::string filepath="../testData/test11.in";
+    std::ifstream ifs;
+    ifs.open(filepath);
+    if (!ifs.good()) {
+        std::cout<<"bad";
+    }
+    ANTLRInputStream input(ifs);
+//    ANTLRInputStream input(std::cin);
     Python3Lexer lexer(&input);
     CommonTokenStream tokens(&lexer);
     tokens.fill();
@@ -24,6 +24,6 @@ int main(int argc, const char* argv[]){
     tree::ParseTree* tree=parser.file_input();
     EvalVisitor visitor;
     visitor.visit(tree);
-//    ifs.close();
+    ifs.close();
     return 0;
 }
